b80623e15cb4b102fdad65ca2cd045a0fe4857d1
Vipulmi <110650383+Vipulmi@users.noreply.github.com>
2024-12-08 00:54:20 +0530

    Update MultKAN.py

--- kan/MultKAN.py
+++ kan/MultKAN.py
@@ -96,219 +96,219 @@ class MultKAN(nn.Module):
     '''
     # def __init__(self, width=None, grid=3, k=3, mult_arity = 2, noise_scale=0.3, scale_base_mu=0.0, scale_base_sigma=1.0, base_fun='silu', symbolic_enabled=True, affine_trainable=False, grid_eps=0.02, grid_range=[-1, 1], sp_trainable=True, sb_trainable=True, seed=1, save_act=True, sparse_init=False, auto_save=True, first_init=True, ckpt_path='./model', state_id=0, round=0, device='cpu'):
     # def __init__(self, width, num_centers=5, sigma=0.1, device='cpu'):
-        '''
-        initalize a KAN model
+    '''
+    initalize a KAN model
+    
+    Args:
+    -----
+        width : list of int
+            Without multiplication nodes: :math:`[n_0, n_1, .., n_{L-1}]` specify the number of neurons in each layer (including inputs/outputs)
+            With multiplication nodes: :math:`[[n_0,m_0=0], [n_1,m_1], .., [n_{L-1},m_{L-1}]]` specify the number of addition/multiplication nodes in each layer (including inputs/outputs)
+        grid : int
+            number of grid intervals. Default: 3.
+        k : int
+            order of piecewise polynomial. Default: 3.
+        mult_arity : int, or list of int lists
+            multiplication arity for each multiplication node (the number of numbers to be multiplied)
+        noise_scale : float
+            initial injected noise to spline.
+        base_fun : str
+            the residual function b(x). Default: 'silu'
+        symbolic_enabled : bool
+            compute (True) or skip (False) symbolic computations (for efficiency). By default: True. 
+        affine_trainable : bool
+            affine parameters are updated or not. Affine parameters include node_scale, node_bias, subnode_scale, subnode_bias
+        grid_eps : float
+            When grid_eps = 1, the grid is uniform; when grid_eps = 0, the grid is partitioned using percentiles of samples. 0 < grid_eps < 1 interpolates between the two extremes.
+        grid_range : list/np.array of shape (2,))
+            setting the range of grids. Default: [-1,1]. This argument is not important if fit(update_grid=True) (by default updata_grid=True)
+        sp_trainable : bool
+            If true, scale_sp is trainable. Default: True.
+        sb_trainable : bool
+            If true, scale_base is trainable. Default: True.
+        device : str
+            device
+        seed : int
+            random seed
+        save_act : bool
+            indicate whether intermediate activations are saved in forward pass
+        sparse_init : bool
+            sparse initialization (True) or normal dense initialization. Default: False.
+        auto_save : bool
+            indicate whether to automatically save a checkpoint once the model is modified
+        state_id : int
+            the state of the model (used to save checkpoint)
+        ckpt_path : str
+            the folder to store checkpoints. Default: './model'
+        round : int
+            the number of times rewind() has been called
+        device : str
         
-        Args:
-        -----
-            width : list of int
-                Without multiplication nodes: :math:`[n_0, n_1, .., n_{L-1}]` specify the number of neurons in each layer (including inputs/outputs)
-                With multiplication nodes: :math:`[[n_0,m_0=0], [n_1,m_1], .., [n_{L-1},m_{L-1}]]` specify the number of addition/multiplication nodes in each layer (including inputs/outputs)
-            grid : int
-                number of grid intervals. Default: 3.
-            k : int
-                order of piecewise polynomial. Default: 3.
-            mult_arity : int, or list of int lists
-                multiplication arity for each multiplication node (the number of numbers to be multiplied)
-            noise_scale : float
-                initial injected noise to spline.
-            base_fun : str
-                the residual function b(x). Default: 'silu'
-            symbolic_enabled : bool
-                compute (True) or skip (False) symbolic computations (for efficiency). By default: True. 
-            affine_trainable : bool
-                affine parameters are updated or not. Affine parameters include node_scale, node_bias, subnode_scale, subnode_bias
-            grid_eps : float
-                When grid_eps = 1, the grid is uniform; when grid_eps = 0, the grid is partitioned using percentiles of samples. 0 < grid_eps < 1 interpolates between the two extremes.
-            grid_range : list/np.array of shape (2,))
-                setting the range of grids. Default: [-1,1]. This argument is not important if fit(update_grid=True) (by default updata_grid=True)
-            sp_trainable : bool
-                If true, scale_sp is trainable. Default: True.
-            sb_trainable : bool
-                If true, scale_base is trainable. Default: True.
-            device : str
-                device
-            seed : int
-                random seed
-            save_act : bool
-                indicate whether intermediate activations are saved in forward pass
-            sparse_init : bool
-                sparse initialization (True) or normal dense initialization. Default: False.
-            auto_save : bool
-                indicate whether to automatically save a checkpoint once the model is modified
-            state_id : int
-                the state of the model (used to save checkpoint)
-            ckpt_path : str
-                the folder to store checkpoints. Default: './model'
-            round : int
-                the number of times rewind() has been called
-            device : str
-            
-        Returns:
-        --------
-            self
-            
-        Example
-        -------
-        >>> from kan import *
-        >>> model = KAN(width=[2,5,1], grid=5, k=3, seed=0)
-        checkpoint directory created: ./model
-        saving model version 0.0
-        '''
-        def __init__(self, width, num_centers=5, sigma=0.1, device='cpu', 
-                 symbolic_enabled=True, affine_trainable=False, auto_save=True, 
-                 ckpt_path='./model', first_init=True, state_id=0, round=0):
-        super(MultKAN, self).__init__()
-
-        torch.manual_seed(0)
-        np.random.seed(0)
-        random.seed(0)
-
-        self.width = width
-        self.depth = len(width) - 1
-        self.device = device
-        self.symbolic_enabled = symbolic_enabled
-        self.affine_trainable = affine_trainable
-        self.auto_save = auto_save
-        self.ckpt_path = ckpt_path
-        self.state_id = state_id
-        self.round = round
-        # Create layers
-        self.layers = nn.ModuleList([
-            KANLayer(
-                in_dim=width[i], 
-                out_dim=width[i+1],
-                num_centers=num_centers,
-                sigma=sigma,
-                device=device
-            ) for i in range(self.depth)
-        ])
-        # ### initializeing the numerical front ###
-
-        # self.act_fun = []
-        # self.depth = len(width) - 1
+    Returns:
+    --------
+        self
         
-        # #print('haha1', width)
-        # for i in range(len(width)):
-        #     #print(type(width[i]), type(width[i]) == int)
-        #     if type(width[i]) == int or type(width[i]) == np.int64:
-        #         width[i] = [width[i],0]
+    Example
+    -------
+    >>> from kan import *
+    >>> model = KAN(width=[2,5,1], grid=5, k=3, seed=0)
+    checkpoint directory created: ./model
+    saving model version 0.0
+    '''
+    def __init__(self, width, num_centers=5, sigma=0.1, device='cpu', 
+             symbolic_enabled=True, affine_trainable=False, auto_save=True, 
+             ckpt_path='./model', first_init=True, state_id=0, round=0):
+    super(MultKAN, self).__init__()
+
+    torch.manual_seed(0)
+    np.random.seed(0)
+    random.seed(0)
+
+    self.width = width
+    self.depth = len(width) - 1
+    self.device = device
+    self.symbolic_enabled = symbolic_enabled
+    self.affine_trainable = affine_trainable
+    self.auto_save = auto_save
+    self.ckpt_path = ckpt_path
+    self.state_id = state_id
+    self.round = round
+    # Create layers
+    self.layers = nn.ModuleList([
+        KANLayer(
+            in_dim=width[i], 
+            out_dim=width[i+1],
+            num_centers=num_centers,
+            sigma=sigma,
+            device=device
+        ) for i in range(self.depth)
+    ])
+    # ### initializeing the numerical front ###
+
+    # self.act_fun = []
+    # self.depth = len(width) - 1
+    
+    # #print('haha1', width)
+    # for i in range(len(width)):
+    #     #print(type(width[i]), type(width[i]) == int)
+    #     if type(width[i]) == int or type(width[i]) == np.int64:
+    #         width[i] = [width[i],0]
+            
+    # #print('haha2', width)
+        
+    # self.width = width
+    
+    # # if mult_arity is just a scalar, we extend it to a list of lists
+    # # e.g, mult_arity = [[2,3],[4]] means that in the first hidden layer, 2 mult ops have arity 2 and 3, respectively;
+    # # in the second hidden layer, 1 mult op has arity 4.
+    # if isinstance(mult_arity, int):
+    #     self.mult_homo = True # when homo is True, parallelization is possible
+    # else:
+    #     self.mult_homo = False # when home if False, for loop is required. 
+    # self.mult_arity = mult_arity
+
+    # width_in = self.width_in
+    # width_out = self.width_out
+    
+    # self.base_fun_name = base_fun
+    # if base_fun == 'silu':
+    #     base_fun = torch.nn.SiLU()
+    # elif base_fun == 'identity':
+    #     base_fun = torch.nn.Identity()
+    # elif base_fun == 'zero':
+    #     base_fun = lambda x: x*0.
+        
+    # self.grid_eps = grid_eps
+    # self.grid_range = grid_range
+        
+    
+    # for l in range(self.depth):
+    #     # splines
+    #     if isinstance(grid, list):
+    #         grid_l = grid[l]
+    #     else:
+    #         grid_l = grid
+            
+    #     if isinstance(k, list):
+    #         k_l = k[l]
+    #     else:
+    #         k_l = k
                 
-        # #print('haha2', width)
-            
-        # self.width = width
         
-        # # if mult_arity is just a scalar, we extend it to a list of lists
-        # # e.g, mult_arity = [[2,3],[4]] means that in the first hidden layer, 2 mult ops have arity 2 and 3, respectively;
-        # # in the second hidden layer, 1 mult op has arity 4.
-        # if isinstance(mult_arity, int):
-        #     self.mult_homo = True # when homo is True, parallelization is possible
-        # else:
-        #     self.mult_homo = False # when home if False, for loop is required. 
-        # self.mult_arity = mult_arity
+    #     sp_batch = KANLayer(in_dim=width_in[l], out_dim=width_out[l+1], num=grid_l, k=k_l, noise_scale=noise_scale, scale_base_mu=scale_base_mu, scale_base_sigma=scale_base_sigma, scale_sp=1., base_fun=base_fun, grid_eps=grid_eps, grid_range=grid_range, sp_trainable=sp_trainable, sb_trainable=sb_trainable, sparse_init=sparse_init)
+    #     self.act_fun.append(sp_batch)
 
-        # width_in = self.width_in
-        # width_out = self.width_out
+    # self.node_bias = []
+    # self.node_scale = []
+    # self.subnode_bias = []
+    # self.subnode_scale = []
+    
+    # globals()['self.node_bias_0'] = torch.nn.Parameter(torch.zeros(3,1)).requires_grad_(False)
+    # exec('self.node_bias_0' + " = torch.nn.Parameter(torch.zeros(3,1)).requires_grad_(False)")
+    
+    # for l in range(self.depth):
+    #     exec(f'self.node_bias_{l} = torch.nn.Parameter(torch.zeros(width_in[l+1])).requires_grad_(affine_trainable)')
+    #     exec(f'self.node_scale_{l} = torch.nn.Parameter(torch.ones(width_in[l+1])).requires_grad_(affine_trainable)')
+    #     exec(f'self.subnode_bias_{l} = torch.nn.Parameter(torch.zeros(width_out[l+1])).requires_grad_(affine_trainable)')
+    #     exec(f'self.subnode_scale_{l} = torch.nn.Parameter(torch.ones(width_out[l+1])).requires_grad_(affine_trainable)')
+    #     exec(f'self.node_bias.append(self.node_bias_{l})')
+    #     exec(f'self.node_scale.append(self.node_scale_{l})')
+    #     exec(f'self.subnode_bias.append(self.subnode_bias_{l})')
+    #     exec(f'self.subnode_scale.append(self.subnode_scale_{l})')
         
-        # self.base_fun_name = base_fun
-        # if base_fun == 'silu':
-        #     base_fun = torch.nn.SiLU()
-        # elif base_fun == 'identity':
-        #     base_fun = torch.nn.Identity()
-        # elif base_fun == 'zero':
-        #     base_fun = lambda x: x*0.
-            
-        # self.grid_eps = grid_eps
-        # self.grid_range = grid_range
-            
-        
-        # for l in range(self.depth):
-        #     # splines
-        #     if isinstance(grid, list):
-        #         grid_l = grid[l]
-        #     else:
-        #         grid_l = grid
-                
-        #     if isinstance(k, list):
-        #         k_l = k[l]
-        #     else:
-        #         k_l = k
-                    
-            
-        #     sp_batch = KANLayer(in_dim=width_in[l], out_dim=width_out[l+1], num=grid_l, k=k_l, noise_scale=noise_scale, scale_base_mu=scale_base_mu, scale_base_sigma=scale_base_sigma, scale_sp=1., base_fun=base_fun, grid_eps=grid_eps, grid_range=grid_range, sp_trainable=sp_trainable, sb_trainable=sb_trainable, sparse_init=sparse_init)
-        #     self.act_fun.append(sp_batch)
+    
+    # self.act_fun = nn.ModuleList(self.act_fun)
 
-        # self.node_bias = []
-        # self.node_scale = []
-        # self.subnode_bias = []
-        # self.subnode_scale = []
-        
-        # globals()['self.node_bias_0'] = torch.nn.Parameter(torch.zeros(3,1)).requires_grad_(False)
-        # exec('self.node_bias_0' + " = torch.nn.Parameter(torch.zeros(3,1)).requires_grad_(False)")
-        
-        # for l in range(self.depth):
-        #     exec(f'self.node_bias_{l} = torch.nn.Parameter(torch.zeros(width_in[l+1])).requires_grad_(affine_trainable)')
-        #     exec(f'self.node_scale_{l} = torch.nn.Parameter(torch.ones(width_in[l+1])).requires_grad_(affine_trainable)')
-        #     exec(f'self.subnode_bias_{l} = torch.nn.Parameter(torch.zeros(width_out[l+1])).requires_grad_(affine_trainable)')
-        #     exec(f'self.subnode_scale_{l} = torch.nn.Parameter(torch.ones(width_out[l+1])).requires_grad_(affine_trainable)')
-        #     exec(f'self.node_bias.append(self.node_bias_{l})')
-        #     exec(f'self.node_scale.append(self.node_scale_{l})')
-        #     exec(f'self.subnode_bias.append(self.subnode_bias_{l})')
-        #     exec(f'self.subnode_scale.append(self.subnode_scale_{l})')
-            
-        
-        # self.act_fun = nn.ModuleList(self.act_fun)
+    # self.grid = grid
+    # self.k = k
+    # self.base_fun = base_fun
 
-        # self.grid = grid
-        # self.k = k
-        # self.base_fun = base_fun
+    ### initializing the symbolic front ###
+    self.symbolic_fun = []
+    for l in range(self.depth):
+        sb_batch = Symbolic_KANLayer(in_dim=width_in[l], out_dim=width_out[l+1])
+        self.symbolic_fun.append(sb_batch)
 
-        ### initializing the symbolic front ###
-        self.symbolic_fun = []
-        for l in range(self.depth):
-            sb_batch = Symbolic_KANLayer(in_dim=width_in[l], out_dim=width_out[l+1])
-            self.symbolic_fun.append(sb_batch)
+    self.symbolic_fun = nn.ModuleList(self.symbolic_fun)
+    self.symbolic_enabled = symbolic_enabled
+    self.affine_trainable = affine_trainable
+    # self.sp_trainable = sp_trainable
+    # self.sb_trainable = sb_trainable
+    
+    self.save_act = save_act
+        
+    self.node_scores = None
+    self.edge_scores = None
+    self.subnode_scores = None
+    
+    self.cache_data = None
+    self.acts = None
+    
+    self.auto_save = auto_save
+    self.state_id = 0
+    self.ckpt_path = ckpt_path
+    self.round = round
+    
+    self.device = device
+    self.to(device)
+    
+    if auto_save:
+        if first_init:
+            if not os.path.exists(ckpt_path):
+                # Create the directory
+                os.makedirs(ckpt_path)
+            print(f"checkpoint directory created: {ckpt_path}")
+            print('saving model version 0.0')
 
-        self.symbolic_fun = nn.ModuleList(self.symbolic_fun)
-        self.symbolic_enabled = symbolic_enabled
-        self.affine_trainable = affine_trainable
-        # self.sp_trainable = sp_trainable
-        # self.sb_trainable = sb_trainable
+            history_path = self.ckpt_path+'/history.txt'
+            with open(history_path, 'w') as file:
+                file.write(f'### Round {self.round} ###' + '\n')
+                file.write('init => 0.0' + '\n')
+            self.saveckpt(path=self.ckpt_path+'/'+'0.0')
+        else:
+            self.state_id = state_id
         
-        self.save_act = save_act
-            
-        self.node_scores = None
-        self.edge_scores = None
-        self.subnode_scores = None
-        
-        self.cache_data = None
-        self.acts = None
-        
-        self.auto_save = auto_save
-        self.state_id = 0
-        self.ckpt_path = ckpt_path
-        self.round = round
-        
-        self.device = device
-        self.to(device)
-        
-        if auto_save:
-            if first_init:
-                if not os.path.exists(ckpt_path):
-                    # Create the directory
-                    os.makedirs(ckpt_path)
-                print(f"checkpoint directory created: {ckpt_path}")
-                print('saving model version 0.0')
-
-                history_path = self.ckpt_path+'/history.txt'
-                with open(history_path, 'w') as file:
-                    file.write(f'### Round {self.round} ###' + '\n')
-                    file.write('init => 0.0' + '\n')
-                self.saveckpt(path=self.ckpt_path+'/'+'0.0')
-            else:
-                self.state_id = state_id
-            
-        self.input_id = torch.arange(self.width_in[0],)
+    self.input_id = torch.arange(self.width_in[0],)
     
         
     def to(self, device):
